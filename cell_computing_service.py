380df5aee098d383ed12bf64fab3136d77cb11e7
alexmorten <alexandrius.martinus@gmail.com>
2019-02-22 00:55:27 +0100

    connection experiment

--- cell_computing_service.py
+++ cell_computing_service.py
@@ -7,6 +7,7 @@ import random
 import time
 import dna_decoding
 import os
+import math
 cell_id_counter = 0
 
 
@@ -17,23 +18,31 @@ class CellComputeServicer(grpc_proto.CellInteractionServiceServicer):
     def ComputeCellInteractions(self, incoming_batch, context):
         global cell_id_counter
         new_cells = []
+        id_to_cell = {}
+        id_to_cell_moved = {}
+        id_to_cell_energy_averaged = {}
+        for c in incoming_batch.cells_to_compute:
+            id_to_cell[str(c.id)] = c
+        for c in incoming_batch.cells_in_proximity:
+            id_to_cell[str(c.id)] = c
+
         # Movement
         for c in incoming_batch.cells_to_compute:
-            c.pos.x += random.uniform(-conf.WORLD_VELOCITY,
-                                      conf.WORLD_VELOCITY)
-            c.pos.y += random.uniform(-conf.WORLD_VELOCITY,
-                                      conf.WORLD_VELOCITY)
-            c.pos.z += random.uniform(-conf.WORLD_VELOCITY,
-                                      conf.WORLD_VELOCITY)
+            move_cell_and_connected_cells(c, id_to_cell, id_to_cell_moved)
         # Interaction
 
         # Energy
         for c in incoming_batch.cells_to_compute:
             f = random.uniform(0, 1)
             if f < dna_decoding.food_theshold(c.dna):
-                c.energy_level += dna_decoding.food_amount(c.dna)
+                food_factor = conf.WANTED_CELL_AMOUNT_PER_BUCKET / \
+                    len(incoming_batch.cells_to_compute)
+                c.energy_level += int(dna_decoding.food_amount(c.dna) * food_factor)
 
             c.energy_level -= conf.GENERAL_ENERGY_CONSUMPTION
+            average_out_energy_in_connected_cells(
+                c, id_to_cell, id_to_cell_energy_averaged)
+
             if c.energy_level > conf.ENERGY_THRESHOLD:
                 new_cells.append(c)
 
@@ -44,16 +53,38 @@ class CellComputeServicer(grpc_proto.CellInteractionServiceServicer):
             if c.energy_level > dna_decoding.division_treshold(c.dna) + cost:
                 c.energy_level -= cost
 
+                child_id = conf.INITIAL_NUMBER_CELLS + cell_id_counter
+                child_connections = []
+
+                if dna_decoding.builds_connection_after_division(
+                    c.dna,
+                    len(c.connections),
+                ):
+                    child_connections.append(proto.Connection(connected_to=c.id))
+                    conn = c.connections.add()
+                    conn.connected_to=child_id
+                    print("++conn++")
+                child_dna = None
+                if dna_decoding.should_sub_slice(c.dna, len(c.connections)):
+                    dna_decoding.dna_sub_slice(c.dna, len(c.connections))
+                else:
+                    child_dna = c.dna
+
+                child_dna = dna_decoding.mutate_dna_with_chance(
+                    c.dna,
+                    conf.MUTATION_CHANCE
+                )
+
                 nc = proto.Cell(
-                    id=conf.INITIAL_NUMBER_CELLS + cell_id_counter,
+                    id=child_id,
                     energy_level=initial_energy,
-                    pos=c.pos,
+                    pos=randomly_shifted_pos(c.pos, 10),
                     vel=proto.Vector(
                         x=0,
                         y=0,
                         z=0),
-                    dna=c.dna,
-                    connections=[])
+                    dna=child_dna,
+                    connections=child_connections)
                 cell_id_counter += 1
                 new_cells.append(nc)
 
@@ -69,7 +100,7 @@ class CellComputeServicer(grpc_proto.CellInteractionServiceServicer):
         for i in range(conf.INITIAL_NUMBER_CELLS):
             initial_position = []
             for j in conf.WORLD_DIMENSION:
-                initial_position.append(random.uniform(0, j))
+                initial_position.append(random.uniform(0, j)+800)
             initial_position = proto.Vector(
                 x=initial_position[0],
                 y=initial_position[1],
@@ -85,3 +116,74 @@ class CellComputeServicer(grpc_proto.CellInteractionServiceServicer):
                 dna=bytes(os.urandom(random.randint(3, 6))),
                 connections=[])
             yield cell
+
+
+def get_value(dict, key):
+    try:
+        value = dict[str(key)]
+        return value, True
+    except:
+        return None, False
+
+
+def move_cell_and_connected_cells(cell, cell_dict, moved_dict):
+    _value, moved = get_value(moved_dict, cell.id)
+    if moved:
+        return
+
+    d_x = random.uniform(-conf.WORLD_VELOCITY, conf.WORLD_VELOCITY)
+    d_y = random.uniform(-conf.WORLD_VELOCITY, conf.WORLD_VELOCITY)
+    d_z = random.uniform(-conf.WORLD_VELOCITY, conf.WORLD_VELOCITY)
+
+    cell_group = group_connected_cells([cell], cell, cell_dict)
+    for g_cell in cell_group:
+        g_cell.pos.x += d_x
+        g_cell.pos.y += d_y
+        g_cell.pos.z += d_z
+        moved_dict[str(g_cell.id)] = True
+
+
+def average_out_energy_in_connected_cells(
+        cell, cell_dict, already_averaged_dict):
+    _, already_averaged = get_value(already_averaged_dict, cell.id)
+    if already_averaged:
+        return
+
+    cell_group = group_connected_cells([cell], cell, cell_dict)
+    energy_sum = 0
+    for c in cell_group:
+        energy_sum += c.energy_level
+
+    for c in cell_group:
+        c.energy_level = int(energy_sum / len(cell_group))
+        already_averaged_dict[str(c.id)] = True
+
+
+def group_connected_cells(group, cell, cell_dict):
+    for conn in cell.connections:
+        other_cell, exists = get_value(cell_dict, conn.connected_to)
+        if exists:
+            if other_cell not in group:
+                group.append(other_cell)
+                group_connected_cells(group, other_cell, cell_dict)
+    return group
+
+def randomly_shifted_pos(pos, shift_dist):
+    d_x, d_y, d_z = random_vector_of_length(shift_dist)
+    return proto.Vector(
+        x=pos.x + d_x,
+        y=pos.y + d_y,
+        z=pos.z + d_z,
+    )
+
+
+def random_vector_of_length(l):
+    x = random.uniform(1 / 10 * 3, 1)
+    y = random.uniform(1 / 10 * 3, 1)
+    z = random.uniform(1 / 10 * 3, 1)
+    # x = 1
+    # y = 0
+    # z = 0
+    dist = math.sqrt(x * x + y * y + z * z)
+    factor = l / dist
+    return x * factor, y * factor, z * factor
--- dna_decoding.py
+++ dna_decoding.py
@@ -1,20 +1,41 @@
 from bitstring import BitArray
 import os
 import cis_config as config
+import random
+
 initial_energy_feature = BitArray(bytes=bytearray(b'\xff'))
 division_treshold_feature = BitArray(bytes=bytearray(b'\xa0'))
 food_treshold_feature = BitArray(bytes=bytearray(b'\x21'))
 food_amount_feature = BitArray(bytes=bytearray(b'\x40'))
+builds_connection_after_division_feature = BitArray(bytes=bytearray(b'\x60'))
+should_sub_slice_feature = BitArray(bytes=bytearray(b'\x10'))
+sub_slice_start_feature = BitArray(bytes=bytearray(b'\x10'))
+sub_slice_start_feature = BitArray(bytes=bytearray(b'\x30'))
+sub_slice_end_feature = BitArray(bytes=bytearray(b'\x50'))
 
 
-def feature_in_dna(dna, feature):
+def feature_in_dna(dna, feature, shift_by=0, start_at=0):
     feature_hit_count = 0
-    for b in dna:
+    feature_copy = feature.copy()
+    start_index = start_at % len(dna)
+    for b in dna[start_index:]:
         bits = BitArray(bytes=[b])
         overlap = bits & feature
         feature_hit_count += overlap.bin.count('1')
+        feature_copy = shift_bits_by(feature_copy, shift_by)
     max_hits = len(dna) * feature.bin.count('1')
     return feature_hit_count / max_hits
+
+
+def feature_value_with_connections(dna, feature, current_connection_count):
+    shift_param = current_connection_count % 8
+    start_at_param = int(current_connection_count / 8) % len(dna)
+    return feature_in_dna(
+        dna,
+        builds_connection_after_division_feature,
+        shift_by=shift_param,
+        start_at=start_at_param
+    )
 
 
 def initial_energy(dna):
@@ -27,9 +48,81 @@ def division_treshold(dna):
         config.DIVISION_THRESHOLD
 
 
+def builds_connection_after_division(dna, current_connection_count):
+    feature_value = feature_value_with_connections(
+        dna,
+        builds_connection_after_division_feature,
+        current_connection_count,
+    )
+    return feature_value < config.CONNECTION_LIKELYHOOD
+
+
 def food_theshold(dna):
     return feature_in_dna(dna, food_treshold_feature) * config.FOOD_THRESHOLD
 
 
 def food_amount(dna):
     return int(feature_in_dna(dna, food_amount_feature) * config.FOOD_ENERGY)
+
+
+def should_sub_slice(dna, current_connection_count):
+    feature_value = feature_value_with_connections(
+        dna,
+        should_sub_slice_feature,
+        current_connection_count
+    )
+    return feature_value < config.DNA_SUBSLICE_CHANCE
+
+
+def dna_sub_slice(dna, current_connection_count):
+    start_feature_value = feature_value_with_connections(
+        dna,
+        sub_slice_start_feature,
+        current_connection_count,
+    )
+    end_feature_value = feature_value_with_connections(
+        dna,
+        sub_slice_end_feature,
+        current_connection_count,
+    )
+    max_start_index = len(dna) - 1
+    start_index = round(start_feature_value * max_start_index)
+    max_end_step = (len(dna) - 1) - start_index
+    end_index = start_index + round(max_end_step * end_feature_value)
+    return dna[start_index:end_index]
+
+
+def shift_bits_by(bits, n):
+    rest = n % 8
+    if rest == 0:
+        return bits
+
+    bit_string = bits.bin
+    shifted_bits = bit_string[rest:] + bit_string[:rest]
+    return BitArray(bin=shifted_bits)
+
+
+def mutate_dna_with_chance(dna, chance):
+    new_dna_bit_string = ''
+    for b in dna:
+        bit_string = BitArray(bytes=[b]).bin
+        if random_bool_with_threshold(chance):
+            bit_string = mutate_bit_string(bit_string)
+        new_dna_bit_string += bit_string
+    if random_bool_with_threshold(chance):
+        new_dna_bit_string += '0'*8
+    return BitArray(bin=new_dna_bit_string).bytes
+
+
+def random_bool_with_threshold(threshold):
+    return random.uniform(0, 1) < threshold
+
+
+def mutate_bit_string(bit_string):
+    random_index = random.randint(0, len(bit_string) - 1)
+    new_bit = ''
+    if bit_string[random_index] == '0':
+        new_bit = '1'
+    else:
+        new_bit = '0'
+    return bit_string[:random_index] + new_bit + bit_string[random_index + 1:]
