6c704948aa377707a6b97b9a0e91f245e8a6b460
Urhengulas <joheihe@web.de>
2019-05-05 19:26:51 +0200

    Order functions in cis_cell.py

--- cis_cell.py
+++ cis_cell.py
@@ -9,25 +9,7 @@ import dna_decoding
 from cis_helper import get_value, group_connected_cells, random_vector_of_length
 
 
-def cells_survive(cells):
-
-    survived_cells = []
-
-    for c in cells:
-        if is_alive(c):
-            survived_cells.append(c)
-
-    return survived_cells
-
-
-def is_alive(cell):
-    starvation_threshold = conf.ENERGY_THRESHOLD
-    if cell.energy_level < starvation_threshold:
-        return False
-    else:
-        return True
-
-
+# energy consumption
 def cells_consume_energy(cells):
 
     for c in cells:
@@ -45,17 +27,58 @@ def consume_energy(cell):
         cell.energy_level = 0
 
 
-def builds_connection_after_division(cell):
-    return dna_decoding.builds_connection_after_division(
-        cell.dna, len(cell.connections))
+# survival
+def cells_survive(cells):
+
+    survived_cells = []
+
+    for c in cells:
+        if is_alive(c):
+            survived_cells.append(c)
+
+    return survived_cells
 
 
-def dna_copy_or_sub_slice(cell):
-    if dna_decoding.dna_should_sub_slice(cell.dna, len(cell.connections)):
-        return dna_decoding.dna_sub_slice(cell.dna, len(cell.connections))
-    return cell.dna
+def is_alive(cell):
+
+    starvation_threshold = conf.ENERGY_THRESHOLD
+    if cell.energy_level < starvation_threshold:
+        return False
+    else:
+        return True
 
 
+# average energy
+def average_out_cell_energy(cells, cell_dict):
+
+    map_id_to_cell_energy_averaged = {}
+
+    for c in cells:
+        average_out_energy_in_connected_cells(
+            c,
+            cell_dict,
+            map_id_to_cell_energy_averaged
+        )
+
+
+def average_out_energy_in_connected_cells(cell, cell_dict, already_averaged_dict):
+
+    _, already_averaged = get_value(already_averaged_dict, cell.id)
+    if already_averaged:
+        return
+
+    cell_group = group_connected_cells([cell], cell, cell_dict)
+
+    energy_sum = 0
+    for c in cell_group:
+        energy_sum += c.energy_level
+
+    for c in cell_group:
+        c.energy_level = int(energy_sum / len(cell_group))
+        already_averaged_dict[c.id] = True
+
+
+# division
 def cells_divide(cells):
 
     for c in cells:
@@ -65,6 +88,7 @@ def cells_divide(cells):
 
 
 def divide(cell):
+
     initial_energy = int(dna_decoding.initial_energy(cell.dna))
     cost = conf.DIVISION_ENERGY_COST + initial_energy
     if cell.energy_level > dna_decoding.division_treshold(cell.dna) + cost:
@@ -96,6 +120,17 @@ def divide(cell):
         return new_cell
 
 
+def builds_connection_after_division(cell):
+    return dna_decoding.builds_connection_after_division(
+        cell.dna, len(cell.connections))
+
+
+def dna_copy_or_sub_slice(cell):
+    if dna_decoding.dna_should_sub_slice(cell.dna, len(cell.connections)):
+        return dna_decoding.dna_sub_slice(cell.dna, len(cell.connections))
+    return cell.dna
+
+
 def randomly_shifted_pos(pos, shift_dist):
     d_x, d_y, d_z = random_vector_of_length(shift_dist)
     return proto.Vector(
@@ -107,28 +142,3 @@ def randomly_shifted_pos(pos, shift_dist):
 
 def random_dna(min_length, max_length):
     return bytes(os.urandom(random.randint(min_length, max_length)))
-
-
-def average_out_energy_in_connected_cells(cell, cell_dict, already_averaged_dict):
-    _, already_averaged = get_value(already_averaged_dict, cell.id)
-    if already_averaged:
-        return
-
-    cell_group = group_connected_cells([cell], cell, cell_dict)
-    energy_sum = 0
-    for c in cell_group:
-        energy_sum += c.energy_level
-
-    for c in cell_group:
-        c.energy_level = int(energy_sum / len(cell_group))
-        already_averaged_dict[c.id] = True
-
-
-def group_connected_cells(group, cell, cell_dict):
-    for conn in cell.connections:
-        other_cell, exists = get_value(cell_dict, conn.connected_to)
-        if exists:
-            if other_cell not in group:
-                group.append(other_cell)
-                group_connected_cells(group, other_cell, cell_dict)
-    return group
